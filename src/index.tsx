b0726d3dc609b806c4d3f03c2d1bf6f7a211e2ca
Marcelo Barbalho <marcelo.barbalho@al.infnet.edu.br>
2021-07-15 19:52:13 -0300

    feita alteração de layout com adição de animação

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-// import App from "./App";
+import GlobalStyle from "./assets/styles/globalStyle";
 import Router from "./routes";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <Router />
   </React.StrictMode>,
   document.getElementById("root")
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 // import { Container } from './styles';
 
@@ -7,8 +7,9 @@ const Router = () => {
   return (
     <>
       <Container>
-        <PageTitle>Promoção</PageTitle>
-        <Description>
+        <PageTitle>BLACK FRIDAY</PageTitle>
+
+        {/* <Description>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam culpa
           expedita, suscipit aspernatur laborum incidunt natus libero totam
           excepturi ratione aliquam tempora obcaecati, ut sit deserunt
@@ -16,8 +17,8 @@ const Router = () => {
         </Description>
         <Wrapper>
           <EmailInput placeholder="digite seu email"></EmailInput>
-          <Button>Send</Button>
-        </Wrapper>
+          <Button>Send</Button> 
+        </Wrapper>*/}
       </Container>
     </>
   );
@@ -32,30 +33,67 @@ const Container = styled.div`
   align-items: center;
   flex-direction: column;
 `;
-const PageTitle = styled.div`
+const glow = keyframes`
+ 0% {
+      color: #f75b30;
+      text-shadow: none;
+    }
+    50% {
+      color: #fff;
+      text-shadow: 0 0 10px #f75b30, 0 0 15px #f75b30, 0 0 20px #f75b30,
+        0 0 25px #f75b30, 0 0 30px #f75b30;
+    }
+    100% {
+      color: #f75b30;
+      text-shadow: none;
+    }
+`;
+const PageTitle = styled.h1`
   height: 300px;
   color: #f75b30;
   font-size: 200px;
-  margin: 0;
-  padding: 0;
+  font-style: italic;
+  letter-spacing: 38px;
+  -webkit-box-reflect: below 1px linear-gradient(transparent, #0007);
+  line-height: 2.2em;
+  animation: ${glow} 5s linear infinite;
+  /* @keyframes glow {
+    0% {
+      color: #f75b30;
+      text-shadow: none;
+    }
+    50% {
+      color: #fff;
+      text-shadow: 0 0 10px #f75b30, 0 0 15px #f75b30, 0 0 20px #f75b30,
+        0 0 25px #f75b30, 0 0 30px #f75b30;
+    }
+    100% {
+      color: #f75b30;
+      text-shadow: none;
+    }
+  } ; */
 `;
 
-const Description = styled.p`
-  color: white;
-  padding: 0 320px;
-  text-align: center;
-  border: auto;
-  border-radius: 5px;
-`;
+// const Description = styled.p`
+//   color: white;
+//   padding: 0 320px;
+//   text-align: center;
+//   border: auto;
+//   border-radius: 5px;
+// `;
 
-const Button = styled.button`
-  background-color: white;
-  color: #59363d;
-  margin: 5px;
-`;
-const EmailInput = styled.input`
-  background-color: #fff;
-`;
-const Wrapper = styled.div`
-  display: flex;
-`;
+// const Button = styled.button`
+//   background-color: white;
+//   color: #59363d;
+//   margin: 5px;
+// `;
+// const EmailInput = styled.input`
+//   background-color: #fff;
+// `;
+// const Wrapper = styled.div`
+//   display: flex;
+// `;
+// const AnimatedTitle = styled.h2`
+//   color: white;
+//   font-size: 100px;
+// `;
